856c240bf3d7d3daefb77bf1d0113863be6fbf4e
hugosenari <hugosenari@gmail.com>
2022-03-12 03:59:02 -0300

    chore: re index platformer

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Writing a 2D Platform Game in Nim with SDL2'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."platformer-master".url = "path:./master";
+  inputs."platformer-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."platformer-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
